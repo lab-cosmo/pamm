097b041ef3edc5b7e7792d27a04d3ecc8580f45e
Piero Gasparotto <piero.gasparotto@gmail.com>
2014-02-19 16:07:11 +0100

    Improved gmm.90: now the output file will be ordered and the first gaussian is the interesting one.

--- src/gmm.f90
+++ src/gmm.f90
@@ -44,11 +44,13 @@
       DOUBLE PRECISION oldloglike  ! Previous logLikelihood
       DOUBLE PRECISION bic         ! Bayesian Inference criteria
       DOUBLE PRECISION aic         ! Akaike Inference criteria
+      DOUBLE PRECISION prif(3)     ! Reference point needed to find out the important gaussian
       INTEGER Nk       ! Number of gaussians in the mixture
       INTEGER delta    ! Number of data to skeep (for a faster calculation)
       INTEGER Nlines   ! Number of lines of the input data file
       INTEGER nsamples ! Number of points used during the calculation
       INTEGER seed     ! seed for the random number generator
+      INTEGER best     ! index of the gaussian of interest
       LOGICAL maxmin ! flag for verbosity
 
       ! Array of Gaussians containing the gaussians parameters
@@ -64,6 +66,7 @@
       CHARACTER*1024 :: cmdbuffer   ! String used for reading text lines from files
       INTEGER ccmd                  ! Index used to control the input parameters
       LOGICAL verbose ! flag for verbosity
+      INTEGER commas(4), par_count  ! stores the index of commas in the parameter string
 
       INTEGER i,j,k,counter,dummyi1 ! Counters and dummy variable
 
@@ -80,6 +83,9 @@
       Nk=-1 ! number of Gaussians in the mixture. Initialized at -1
       seed=1357
       test=111
+      prif(1)=-1.0d0
+      prif(2)= 2.9d0
+      prif(3)= 2.9d0
       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
       !!!!!!! Command line parser !!!!!!!!!!!!!
@@ -101,6 +107,8 @@
             ccmd = 7
          ELSEIF (cmdbuffer == "-seed") THEN   ! seed for the random number genarator
             ccmd = 8
+         ELSEIF (cmdbuffer == "-rif") THEN    ! point from wich calculate the distances to order
+            ccmd = 9                          ! the gaussians in the output
          ELSEIF (cmdbuffer == "-maxmin") THEN ! initialize using maxmin routine
             maxmin = .true.
          ELSEIF (cmdbuffer == "-h") THEN      ! help
@@ -132,10 +140,22 @@
                READ(cmdbuffer,*) errc
             ELSEIF (ccmd == 8) THEN ! read the seed
                READ(cmdbuffer,*) seed
+            ELSEIF (ccmd == 9) THEN ! vrif,wrif,dADrif
+               par_count = 1
+               commas(1) = 0
+               DO WHILE (index(cmdbuffer(commas(par_count)+1:), ',') > 0)
+                  commas(par_count + 1) = index(cmdbuffer(commas(par_count)+1:), ',') + commas(par_count)
+                  READ(cmdbuffer(commas(par_count)+1:commas(par_count + 1)-1),*) prif(par_count)
+                  par_count = par_count + 1
+               ENDDO
+               READ(cmdbuffer(commas(par_count)+1:),*) prif(par_count)
             ENDIF
          ENDIF
       ENDDO
       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      
+      write(*,*) prif
+      CALL EXIT(-1)
 
       CALL SRAND(seed) ! initialize the random number generator
 
@@ -219,6 +239,8 @@
             WRITE(*,*) " AIC: ", aic
          ENDIF
       ENDDO
+      
+      best=bestgaussian(Nk,clusters,prif)
 
       OPEN(UNIT=11,FILE=outputfile,STATUS='REPLACE',ACTION='WRITE')
       WRITE(11,"(A13,I11,A17,F16.7,A6,F16.7,A6,F16.7)") "# n.samples: ", nsamples, &
@@ -226,14 +248,12 @@
                                                          bic/nsamples, " aic: ", aic/nsamples
       WRITE(11,*) "# mean cov pk"
       WRITE(11,*) Nk
+      ! Write the first the most important gaussian
+      CALL writegausstofile(11,clusters(best),lpks(best))
+      ! and now all the others
       DO k=1,Nk      ! write mean
-         WRITE(11,*) clusters(k)%mean(1)," ",clusters(k)%mean(2)," ",clusters(k)%mean(3)," ", &
-                     ! write covariance matrix
-                     clusters(k)%cov(1,1)," ",clusters(k)%cov(1,2)," ",clusters(k)%cov(1,3)," ", &
-                     clusters(k)%cov(2,1)," ",clusters(k)%cov(2,2)," ",clusters(k)%cov(2,3)," ", &
-                     clusters(k)%cov(3,1)," ",clusters(k)%cov(3,2)," ",clusters(k)%cov(3,3)," ", &
-                     ! write Pk of the gaussian
-                     dexp(lpks(k))
+         IF(k.EQ.best) CYCLE
+         CALL writegausstofile(11,clusters(k),lpks(k))
       ENDDO
       CLOSE(UNIT=11)
 
@@ -248,7 +268,7 @@
             WRITE(*,*) ""
             WRITE(*,*) " SYNTAX: gmm [-h] [-v] -i filename -n gaussians number [-seed seedrandom] "
             WRITE(*,*) "             [-o outputfile] [-ev delta] [-err error] [-s smoothing_factor] "
-            WRITE(*,*) "             [-gf gaussianfile] [-maxmin] "
+            WRITE(*,*) "             [-gf gaussianfile] [-maxmin] [-rif vrif,wrif,dADrif]"
             WRITE(*,*) ""
          END SUBROUTINE helpmessage
 
@@ -273,6 +293,28 @@
             CALL gauss_prepare(gaussp)
 
          END SUBROUTINE readgaussfromfile
+         
+         SUBROUTINE writegausstofile(fileid,gaussp,lpk)
+            ! Read a line from the file and get the paramters for the related gaussian
+            !
+            ! Args:
+            !    fileid: the file containing the gaussians parameters
+            !    gaussp: type_gaussian container in wich we store the gaussian parameters
+            !    lpk: logarithm of the Pk associated to the gaussian
+
+            INTEGER, INTENT(IN) :: fileid
+            TYPE(gauss_type) , INTENT(INOUT) :: gaussp
+            DOUBLE PRECISION, INTENT(INOUT) :: lpk
+            
+            WRITE(fileid,*) gaussp%mean(1)," ",gaussp%mean(2)," ",gaussp%mean(3)," ", &
+                            ! write covariance matrix
+                            gaussp%cov(1,1)," ",gaussp%cov(1,2)," ",gaussp%cov(1,3)," ", &
+                            gaussp%cov(2,1)," ",gaussp%cov(2,2)," ",gaussp%cov(2,3)," ", &
+                            gaussp%cov(3,1)," ",gaussp%cov(3,2)," ",gaussp%cov(3,3)," ", &
+                            ! write Pk of the gaussian
+                            dexp(lpk)
+
+         END SUBROUTINE writegausstofile
 
          SUBROUTINE generatefromscratch(maxmin,nsamples,vwda,ng,clusters,lpks)
             ! Iniatialize from scratch. Guess starting values for
@@ -379,5 +421,32 @@
             cmdbuffer="rm tmp.tmp"
             CALL system(cmdbuffer)
          END FUNCTION
+         
+         INTEGER FUNCTION bestgaussian(ng,clusters,prif)
+            ! Return the index of the gaussian closest to prif
+            !
+            ! Args:
+            !    ng: number of gaussians to generate
+            !    clusters: array containing gaussians parameters
+            !    prif: reference point
+            
+            INTEGER, INTENT(IN) :: ng
+            TYPE(gauss_type), DIMENSION(ng), INTENT(INOUT) :: clusters
+            DOUBLE PRECISION, DIMENSION(3), INTENT(IN) :: prif
+            
+            DOUBLE PRECISION old,new
+			INTEGER i
+			
+			old=dsqrt(dot_product(clusters(1)%mean, prif))
+			bestgaussian=1
+			DO i=2,ng
+			   new=dsqrt(dot_product(clusters(i)%mean, prif))
+			   IF(new.LT.old)THEN
+			      old=new
+			      bestgaussian=i
+			   ENDIF			   
+            ENDDO
+
+         END FUNCTION bestgaussian
 
       END PROGRAM gmm
