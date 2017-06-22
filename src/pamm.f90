fd096aca35011f4b5bfd7dcdbec6ff7a41d30efa
robeme <rmei@uni-bremen.de>
2017-06-22 16:58:37 +0200

    fixed small glitch in QS cutoff

--- src/pamm.f90
+++ src/pamm.f90
@@ -712,7 +712,7 @@
          counter=1
          DO WHILE(qspath(counter).NE.idxroot(qspath(counter)))
             idxroot(qspath(counter)) = qs_next(ngrid,qspath(counter),prob,distmm, &
-                                               sigma2(qspath(counter))*(DSQRT(Di(i)+1.0d0)**2)*qscut2)   
+                                               sigma2(qspath(counter))*(DSQRT(Di(i))+1.0d0)**2)*qscut)   
             IF(idxroot(idxroot(qspath(counter))).NE.0) EXIT
             counter=counter+1
             qspath(counter)=idxroot(qspath(counter-1))
